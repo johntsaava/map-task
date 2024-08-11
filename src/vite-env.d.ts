9f011650191704f71eb37633922e14b400d91453
John Tsaava <johntsaava@gmail.com>
2024-08-11 12:41:28 +0400

    connect to supabase

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_MAPBOX_ACCESS_TOKEN: string;
   readonly VITE_MAPBOX_STYLE: string;
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
 }
 
 interface ImportMeta {
